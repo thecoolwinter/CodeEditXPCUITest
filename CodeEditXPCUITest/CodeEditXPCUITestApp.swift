78a1db6475ca9a279ec1623e0c16eba97babc3d7
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2022-11-10 19:28:53 -0600

    Initial Commit

---
+++ CodeEditXPCUITest/CodeEditXPCUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeEditXPCUITestApp.swift
+//  CodeEditXPCUITest
+//
+//  Created by Khan Winter on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CodeEditXPCUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
